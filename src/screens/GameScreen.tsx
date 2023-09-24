e49daeeab4fdc9a2768058ae4bcea5909fb45109
Scott-Kinzer <zaz765831@gmail.com>
2023-09-24 11:01:11 +0300

    feat: implemented word guess request with hints

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -2,12 +2,17 @@ import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import WordView from '../components/word-view/WordView';
 import CustomKeyboard from '../components/keyboard/CustomKeyboard';
-import {useQuery} from '@tanstack/react-query';
+import {useMutation, useQuery} from '@tanstack/react-query';
 import {
   WordDetailsResponse,
+  WordGuessRequest,
+  WordHintRequest,
   wordDetailsRequest,
+  wordGuessRequest,
+  wordhintRequest,
 } from '../services/game.service';
 import {GameProps} from '../types/route.screen.types';
+import {AxiosError} from 'axios';
 
 const GameScreen = ({route}: GameProps) => {
   const [userAnswer, setUserAnswer] = useState('');
@@ -15,16 +20,68 @@ const GameScreen = ({route}: GameProps) => {
     undefined | WordDetailsResponse
   >();
   const isValid = wordDetails?.wordLength === userAnswer.length;
+  const hintstLeft = wordDetails
+    ? wordDetails?.maxLevelHint - wordDetails?.hint.levelHint
+    : 0;
 
-  console.log(route.params.wordId);
+  const [isAttemptsEnded, setIsAttemptsEnded] = useState(false);
 
-  const {} = useQuery(
+  useQuery(
     ['wordDetails'],
     () => wordDetailsRequest({wordId: route.params.wordId}),
     {
       onSuccess(data) {
-        console.log(data);
         setWordDetails(data);
+      },
+    },
+  );
+
+  const {mutate: mutateHint} = useMutation(
+    (data: WordHintRequest) => wordhintRequest(data),
+    {
+      onSuccess(data) {
+        setWordDetails(prev => {
+          if (prev) {
+            return {
+              ...prev,
+              hint: {
+                ...data,
+              },
+            };
+          }
+        });
+      },
+    },
+  );
+
+  const {mutate} = useMutation(
+    (data: WordGuessRequest) => wordGuessRequest(data),
+    {
+      onSuccess() {
+        console.log('you guessed');
+      },
+      onError(error) {
+        const errorAxios = (error as AxiosError).response?.data as {
+          message: string;
+        };
+
+        if (
+          errorAxios?.message === 'Wrong word' &&
+          wordDetails &&
+          wordDetails?.hint.levelHint < wordDetails?.maxLevelHint
+        ) {
+          mutateHint({
+            wordId: wordDetails.id,
+            level: wordDetails.hint.levelHint + 1,
+          });
+        } else if (
+          errorAxios?.message === 'Wrong word' &&
+          wordDetails &&
+          wordDetails?.hint.levelHint === wordDetails?.maxLevelHint
+        ) {
+          setIsAttemptsEnded(true);
+          console.log('you loose');
+        }
       },
     },
   );
@@ -46,12 +103,19 @@ const GameScreen = ({route}: GameProps) => {
   };
 
   const submit = () => {
-    console.log(userAnswer);
+    if (wordDetails && !isAttemptsEnded) {
+      mutate({
+        word: userAnswer.toLowerCase(),
+        wordId: wordDetails?.id,
+      });
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Let's play</Text>
+      <Text style={styles.title}>Hints left {hintstLeft}</Text>
+
       {wordDetails && (
         <>
           <View style={styles.wordViewWrapper}>
